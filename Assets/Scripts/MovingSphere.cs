5a75387814ecf98ca460b2b6b52ef74589ea790f
D. Azevedo <rplazevedo@gmail.com>
2024-07-31 13:35:01 +0100

    Contrained movement

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -5,6 +5,8 @@ public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
+    [SerializeField] Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
     Vector3 velocity;
     private void Update()
     {
@@ -14,11 +16,17 @@ public class MovingSphere : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         Vector3 desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);  
-
+        
         Vector3 displacement = velocity * Time.deltaTime;
-        transform.localPosition += displacement;
+        Vector3 newPosition = transform.localPosition + displacement;
+        if (!allowedArea.Contains(new Vector2 (newPosition.x, newPosition.z)))
+        {
+            newPosition = transform.localPosition;
+        }
+        transform.localPosition = newPosition;
     }
 }
