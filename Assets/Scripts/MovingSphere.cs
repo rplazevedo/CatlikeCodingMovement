5ca82643a49434384ce3fc6ddd4d4974d7f90abf
D. Azevedo <rplazevedo@gmail.com>
2024-07-31 13:20:30 +0100

    Trying acceleration

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    Vector3 velocity;
     private void Update()
     {
         Vector2 playerInput;
+        //Vector3 velocity;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 velocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        Vector3 acceleration = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        velocity += acceleration * Time.deltaTime;
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
