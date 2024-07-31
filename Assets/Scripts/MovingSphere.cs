cbb7bf21668bd082474fad8018adf69413ce70c4
D. Azevedo <rplazevedo@gmail.com>
2024-07-31 13:16:21 +0100

    Usingn displacement and velocity

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     private void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        transform.localPosition = new Vector3(playerInput.x, 0.5f,playerInput.y);
+        Vector3 velocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
     }
 }
