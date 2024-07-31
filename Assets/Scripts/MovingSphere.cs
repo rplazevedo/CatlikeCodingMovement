2b18300140c0e357e866b33d9095c90369c0583d
D. Azevedo <rplazevedo@gmail.com>
2024-07-31 13:25:01 +0100

    Manual implementation of MoveTowards

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
     Vector3 velocity;
     private void Update()
     {
@@ -11,8 +12,17 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 acceleration = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
-        velocity += acceleration * Time.deltaTime;
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        if (velocity.x < desiredVelocity.x)
+        {
+            velocity.x = Mathf.Min(velocity.x + maxSpeedChange, desiredVelocity.x);
+        }
+        else if (velocity.x > desiredVelocity.x)
+            {
+                velocity.x = Mathf.Min(velocity.x - maxSpeedChange, desiredVelocity.x);
+            }
+
         Vector3 displacement = velocity * Time.deltaTime;
         transform.localPosition += displacement;
     }
