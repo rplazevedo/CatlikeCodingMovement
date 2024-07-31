a885ca746bd7aa3fef1f317daa8dc00e93a4a950
D. Azevedo <rplazevedo@gmail.com>
2024-07-31 13:08:12 +0100

    Control postion manually

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-
+    private void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        transform.localPosition = new Vector3(playerInput.x, 0.5f,playerInput.y);
+    }
 }
