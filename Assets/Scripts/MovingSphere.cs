c2a5f3849f417a83c202b5d40eec1e3a2e110213
D. Azevedo <rplazevedo@gmail.com>
2024-08-01 14:35:44 +0100

    Started Physics tutorial

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -5,8 +5,6 @@ public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
-    [SerializeField, Range(0f, 1)] float bounciness = 0.5f;
-    [SerializeField] Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
 
     Vector3 velocity;
     private void Update()
@@ -24,26 +22,7 @@ public class MovingSphere : MonoBehaviour
         
         Vector3 displacement = velocity * Time.deltaTime;
         Vector3 newPosition = transform.localPosition + displacement;
-        if (newPosition.x < allowedArea.xMin)
-        {
-            newPosition.x = allowedArea.xMin;
-            velocity.x = -velocity.x * bounciness;
-        }
-        else if (newPosition.x > allowedArea.xMax)
-        {
-            newPosition.x = allowedArea.xMax;
-            velocity.x = -velocity.x * bounciness;
-        }
-        if (newPosition.z < allowedArea.yMin)
-        {
-            newPosition.z = allowedArea.yMin;
-            velocity.z = -velocity.z * bounciness;
-        }
-        else if (newPosition.z > allowedArea.yMax)
-        {
-            newPosition.z = allowedArea.yMax;
-            velocity.z = -velocity.z * bounciness;
-        }
+        
         transform.localPosition = newPosition;
     }
 }
