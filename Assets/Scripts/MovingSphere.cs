fdb0338d1e0f3510ded56cd3a30faf65a21d77d6
D. Azevedo <rplazevedo@gmail.com>
2024-08-02 14:23:54 +0100

    Split up movement and inputs

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -5,24 +5,29 @@ public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
+    
+    Vector3 velocity, desiredVelocity;
+    Rigidbody body;
 
-    Vector3 velocity;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
         Vector2 playerInput;
-        //Vector3 velocity;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+    }
 
+    private void FixedUpdate()
+    {
+        velocity = body.velocity;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);  
-        
-        Vector3 displacement = velocity * Time.deltaTime;
-        Vector3 newPosition = transform.localPosition + displacement;
-        
-        transform.localPosition = newPosition;
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);    
+        body.velocity = velocity;
     }
 }
