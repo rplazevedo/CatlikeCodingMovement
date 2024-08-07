0b79b6790c27f09b5b25dbcb906d94e612403361
D. Azevedo <rplazevedo@gmail.com>
2024-08-07 13:47:56 +0100

    Added jumping

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -5,9 +5,11 @@ public class MovingSphere : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxAcceleration = 10f;
-    
+    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+
     Vector3 velocity, desiredVelocity;
     Rigidbody body;
+    bool desiredJump;
 
     private void Awake()
     {
@@ -20,6 +22,8 @@ public class MovingSphere : MonoBehaviour
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+
+        desiredJump |= Input.GetButtonDown("Jump");
     }
 
     private void FixedUpdate()
@@ -28,6 +32,18 @@ public class MovingSphere : MonoBehaviour
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);    
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+
         body.velocity = velocity;
+    }
+
+    void Jump()
+    {
+        velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
     }
 }
