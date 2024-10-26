a310af488536444d60342a5b0b9349e69b65b9c4
D. Azevedo <rplazevedo@gmail.com>
2024-10-26 18:29:00 +0100

    NOrmalized speed

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -62,7 +62,13 @@ public class MovingSphere : MonoBehaviour
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         if (playerInputSpace)
         {
-            desiredVelocity = playerInputSpace.TransformDirection(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
         }
         else
         {
