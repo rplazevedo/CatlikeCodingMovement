18e7acba8b712ce5e5859a9ace246876db240659
D. Azevedo <rplazevedo@gmail.com>
2025-03-11 19:16:53 +0000

    Added opption to toggle floatToSleep

--- Assets/Scripts/CustomGravityRigidbody.cs
+++ Assets/Scripts/CustomGravityRigidbody.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class CustomGravityRigidbody : MonoBehaviour
 {
+    [SerializeField]
+    bool floatToSleep = false;
+
     private Rigidbody body;
     private Material material;
-    public float floatDelay;
+    private float floatDelay;
 
     private void Awake()
     {
@@ -16,23 +19,26 @@ public class CustomGravityRigidbody : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (body.IsSleeping())
+        if (floatToSleep)
         {
-            floatDelay = 0f;
-            return;
-        }
-
-        if (body.velocity.sqrMagnitude < 0.0001f)
-        {
-            floatDelay += Time.deltaTime;
-            if (floatDelay >= 1f)
+            if (body.IsSleeping())
             {
+                floatDelay = 0f;
                 return;
             }
-        }
-        else
-        {
-            floatDelay = 0f;
+
+            if (body.velocity.sqrMagnitude < 0.0001f)
+            {
+                floatDelay += Time.deltaTime;
+                if (floatDelay >= 1f)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                floatDelay = 0f;
+            }
         }
         body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
     }
@@ -43,7 +49,6 @@ public class CustomGravityRigidbody : MonoBehaviour
         {
             material.SetFloat("_FloatDelay", floatDelay);
             material.SetInt("_IsSleeping", body.IsSleeping() ? 1 : 0); // Pass sleeping state to shader
-            Debug.Log("FloatDelay Updated: " + floatDelay);
         }
     }
 }
